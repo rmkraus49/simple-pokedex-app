8d6f7cf55fc164dbde5bf9d9f63f18d52050e473
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-03-01 21:54:25 -0500

    wrapped repository in IIFE; added pokemon entry validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,95 @@
-var repository = []
+var pokemonRepository = (function () {
+  var repository = [];
+
+  return {
+    add: function(pokemon) {
+      if(
+          (typeof pokemon) === 'object' &
+          (typeof pokemon.number) === 'number' &
+          (typeof pokemon.name) === 'string' &
+          (typeof pokemon.height) === 'number' &
+          (typeof pokemon.types) === 'object'
+        ) {
+        repository.push(pokemon);
+      } else {
+        console.log('pokedex entry for ' + pokemon.name + ' failed data type validation');
+        console.log('pokemon data type: ' + typeof pokemon);
+        console.log('pokemon number data type: ' + typeof pokemon.number);
+        console.log('pokemon name data type: ' + typeof pokemon.name);
+        console.log('pokemon height data type: ' + typeof pokemon.height);
+        console.log('pokemon type data type: ' + typeof pokemon.types);
+      }
+    },
+    getAll: function() {
+      return repository;
+    }
+  };
+})();
 
 // repository:
 
-repository[0] = {
+pokemonRepository.add({
   number: 1,
   name: 'Bulbasaur',
   height: 0.7,
   types: ['Grass', 'Poison']
-}
+})
 
-repository[1] = {
+pokemonRepository.add({
   number: 2,
   name: 'Ivysaur',
   height: 1,
   types: ['Grass', 'Poison']
-}
+})
 
-repository[2] = {
+pokemonRepository.add({
   number: 3,
   name: 'Venusaur',
   height: 2,
   types: ['Grass', 'Poison']
-}
+})
 
-repository[3] = {
+pokemonRepository.add({
   number: 4,
   name: 'Charmander',
   height: 0.6,
   types: ['Fire']
-}
+})
 
-repository[4] = {
+pokemonRepository.add({
   number: 5,
   name: 'Charmeleon',
   height: 1.1,
   types: ['Fire']
-}
+})
 
-repository[5] = {
+pokemonRepository.add({
   number: 6,
   name: 'Charizard',
   height: 1.7,
   types: ['Fire', 'Flying']
-}
+})
 
-repository[6] = {
+pokemonRepository.add({
   number: 7,
   name: 'Squirtle',
   height: 0.5,
   types: ['Water']
-}
+})
 
-repository[7] = {
+pokemonRepository.add({
   number: 8,
   name: 'Wartortle',
   height: 1,
   types: ['Water']
-}
+})
 
-repository[8] = {
+pokemonRepository.add({
   number: 9,
   name: 'Blastoise',
   height: 1.6,
   types: ['Water']
-}
+})
 
 function printPokemonDetails (pokemon) {
   document.write('<div>' + pokemon.name + ' (Height: ' + pokemon.height + 'm)')
@@ -76,4 +102,4 @@ function printPokemonDetails (pokemon) {
   document.write('</div>')
 }
 
-repository.forEach(printPokemonDetails)
+pokemonRepository.getAll().forEach(printPokemonDetails)
