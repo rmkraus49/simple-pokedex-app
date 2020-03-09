f10750d0d26d348d326b7a4311527b04880beb8d
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-03-09 00:09:32 -0400

    added call to PokeAPI

--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,85 @@ var $pokemonList = document.querySelector('.pokemon-list');
 
 var pokemonRepository = (function () {
   var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  return {
-    add: function(pokemon) {
-      if(
-          (typeof pokemon) === 'object' &
-          (typeof pokemon.number) === 'number' &
-          (typeof pokemon.name) === 'string' &
-          (typeof pokemon.height) === 'number' &
-          (typeof pokemon.types) === 'object'
-        ) {
-        repository.push(pokemon);
-      } else {
-        console.log('pokedex entry for ' + pokemon.name + ' failed data type validation');
-        console.log('pokemon data type: ' + typeof pokemon);
-        console.log('pokemon number data type: ' + typeof pokemon.number);
-        console.log('pokemon name data type: ' + typeof pokemon.name);
-        console.log('pokemon height data type: ' + typeof pokemon.height);
-        console.log('pokemon type data type: ' + typeof pokemon.types);
-      }
-    },
-    getAll: function() {
-      return repository;
-    },
-    addListItem: function(pokemon) {
-      var listItem = document.createElement('li');
-      var button = document.createElement('button');
-      button.innerText = pokemon.name;
-      button.classList.add('pokemon-button');
-      listItem.appendChild(button);
-      $pokemonList.appendChild(listItem);
-      button.addEventListener('click', function(event) {
-        pokemonRepository.showDetails(pokemon);
-      })
-    },
-    showDetails: function(pokemon) {
-      console.log(pokemon.name);
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function (item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  };
+
+  function add(pokemon) {
+    if(
+        (typeof pokemon) === 'object' &
+        (typeof pokemon.number) === 'number' &
+        (typeof pokemon.name) === 'string' &
+        (typeof pokemon.height) === 'number' &
+        (typeof pokemon.types) === 'object'
+      ) {
+      repository.push(pokemon);
+    } else {
+      console.log('pokedex entry for ' + pokemon.name + ' failed data type validation');
+      console.log('pokemon data type: ' + typeof pokemon);
+      console.log('pokemon number data type: ' + typeof pokemon.number);
+      console.log('pokemon name data type: ' + typeof pokemon.name);
+      console.log('pokemon height data type: ' + typeof pokemon.height);
+      console.log('pokemon type data type: ' + typeof pokemon.types);
     }
   };
+
+  function getAll() {
+    return repository;
+  };
+
+  function addListItem(pokemon) {
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem);
+    button.addEventListener('click', function(event) {
+      pokemonRepository.showDetails(pokemon);
+    })
+  };
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  };
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  return {
+    add: add,
+    addListItem: addListItem,
+    getAll: getAll,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
+  }
 })();
 
 var $mainTitle = document.querySelector('h1');
