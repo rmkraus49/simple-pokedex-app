57841a6b547957c7eddd147d76939a0b8127fc23
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-03-09 13:10:20 -0400

    udpated API call, removed old repository

--- js/scripts.js
+++ js/scripts.js
@@ -20,22 +20,16 @@ var pokemonRepository = (function () {
     })
   };
 
+//adds pokemon from API to repository after data type validation
   function add(pokemon) {
     if(
         (typeof pokemon) === 'object' &
-        (typeof pokemon.number) === 'number' &
         (typeof pokemon.name) === 'string' &
-        (typeof pokemon.height) === 'number' &
-        (typeof pokemon.types) === 'object'
+        (typeof pokemon.detailsUrl) === 'string'
       ) {
       repository.push(pokemon);
     } else {
       console.log('pokedex entry for ' + pokemon.name + ' failed data type validation');
-      console.log('pokemon data type: ' + typeof pokemon);
-      console.log('pokemon number data type: ' + typeof pokemon.number);
-      console.log('pokemon name data type: ' + typeof pokemon.name);
-      console.log('pokemon height data type: ' + typeof pokemon.height);
-      console.log('pokemon type data type: ' + typeof pokemon.types);
     }
   };
 
@@ -83,73 +77,11 @@ var pokemonRepository = (function () {
   }
 })();
 
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
+
 var $mainTitle = document.querySelector('h1');
 $mainTitle.innerText = 'Pokedex';
-
-// repository:
-
-pokemonRepository.add({
-  number: 1,
-  name: 'Bulbasaur',
-  height: 0.7,
-  types: ['Grass', 'Poison']
-})
-
-pokemonRepository.add({
-  number: 2,
-  name: 'Ivysaur',
-  height: 1,
-  types: ['Grass', 'Poison']
-})
-
-pokemonRepository.add({
-  number: 3,
-  name: 'Venusaur',
-  height: 2,
-  types: ['Grass', 'Poison']
-})
-
-pokemonRepository.add({
-  number: 4,
-  name: 'Charmander',
-  height: 0.6,
-  types: ['Fire']
-})
-
-pokemonRepository.add({
-  number: 5,
-  name: 'Charmeleon',
-  height: 1.1,
-  types: ['Fire']
-})
-
-pokemonRepository.add({
-  number: 6,
-  name: 'Charizard',
-  height: 1.7,
-  types: ['Fire', 'Flying']
-})
-
-pokemonRepository.add({
-  number: 7,
-  name: 'Squirtle',
-  height: 0.5,
-  types: ['Water']
-})
-
-pokemonRepository.add({
-  number: 8,
-  name: 'Wartortle',
-  height: 1,
-  types: ['Water']
-})
-
-pokemonRepository.add({
-  number: 9,
-  name: 'Blastoise',
-  height: 1.6,
-  types: ['Water']
-})
-
-// forEach loop to display list of pokemon:
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
